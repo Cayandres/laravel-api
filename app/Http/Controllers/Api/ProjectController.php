fba0e9405eb22875056d50fa07c1f2ba6480a684
Andres Cayambe <andrescayambe99@hotmail.com>
2023-06-28 18:34:11 +0200

    [add]milestone 1-2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+        $projects = Project::all();
+
+        return response()->json($projects);
+    }
+}
